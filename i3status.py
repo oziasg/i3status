3fa6c1ce84c899cb6168195a6c87939f583f03e5
Ozias Gonet <oziasgonet@gmail.com>
2017-08-17 15:38:32 -0400

    Updated for max brightness.

--- i3status.py
+++ i3status.py
@@ -1,54 +1,50 @@
 import json
 import subprocess
 
-# get process output string 
 def get_process(process): 
+    '''get the process output string'''
     return str(subprocess.check_output(process).decode("ascii")).strip() 
 
-# get brightness percentage 
-def get_brightness(): 
-    # max brightness on my system is 852    
-    max_brightness = 852
-    file_name = "/sys/class/backlight/intel_backlight/brightness" 
+def get_brightness_int_from_filename(filename): 
+    _path = "/sys/class/backlight/intel_backlight/" + filename
+    with open(_path, 'r') as file:
+        return int(file.read().replace('\n', '')) 
 
-    with open(file_name, 'r') as file:
-        brightness = file.read().replace('\n', '') 
+def get_brightness_percentage(): 
+    _max = get_brightness_int_from_filename("max_brightness") 
+    _curr = get_brightness_int_from_filename("brightness") 
+    _percentage = round(_curr / _max * 10) * 10 
+    return "BRIGHTNESS {0}%".format(_percentage) 
 
-    percentage = round(int(brightness) / max_brightness * 10) * 10 
-    return "BRIGHTNESS {0}%".format(percentage) 
-
-
-# get the next line of output from the process and format it 
 def next_line(i3status): 
+    '''get the next line of output from the process and format it'''
     line = str(i3status.stdout.readline().decode("ascii")).strip()
     return line
 
-# add data to be displayed on the i3 menu
 def insert_custom_objects(json_line): 
-    json_line.insert(7, {'full_text' : get_brightness(), 'name' : 'brightness'})
+    '''insert a custom object into the i3 tick''' 
+    json_line.insert(7, {'full_text' : get_brightness_percentage(), 'name' : 'brightness'})
     return json_line
 
-# prints the i3status JSON output tick
-def print_json(i3status): 
-    line = next_line(i3status)
-    # remove the comma at the beginning 
-    line = line[1:] 
-   
-    json_line = json.loads(line) 
-    json_line = insert_custom_objects(json_line) 
-    print("," + json.dumps(json_line), flush=True) 
+def print_JSON_tick(i3status): 
+    # gets the next line without the initial comma 
+    _json_line = json.loads(next_line(i3status)[1:]) 
+    _json_line = insert_custom_objects(_json_line) 
+    print("," + json.dumps(_json_line), flush=True) 
 
-def main(): 
-    # pipe i3status output 
-    i3status = subprocess.Popen("i3status", stdout=subprocess.PIPE)
+def get_piped_i3status(): 
+    return subprocess.Popen("i3status", stdout=subprocess.PIPE)
 
-    # first 3 lines can be skipped 
+def print_first_3_lines(i3status): 
+    '''The first 3 lines can be skipped.''' 
     for x in range(0,3): 
         print(next_line(i3status)) 
-   
-    # prints the i3status JSON output tick in an infinite loop
+
+def init_tick(i3status): 
     while True: 
-        print_json(i3status)    
+        print_JSON_tick(i3status)    
 
-
-main() 
+if __name__ == "__main__": 
+    _i3status = get_piped_i3status() 
+    print_first_3_lines(_i3status) 
+    init_tick(_i3status) 
